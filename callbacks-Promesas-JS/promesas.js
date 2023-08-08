4170682b996d124d122160243eae6d86a8226296
KateClysm <kate.clysm@gmail.com>
2023-08-08 00:20:02 -0300

    Limpieza y organicación

--- callbacks-Promesas-JS/promesas.js
+++
@@ -1,7 +0,0 @@
-const miPromesa = new Promise (  (resolve, reject) => {
-    setTimeout(() => {
-        resolve('este es el valor que eventualmente devolverá la promesa');
-    }, 300);
-})
-
-console.log(miPromesa);
